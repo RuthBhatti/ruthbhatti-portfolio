114e159e4d07bf0f6db6d6d5e450fef1f300ba8d
Ruth Bhatti <168582855+RuthBhatti@users.noreply.github.com>
2024-08-12 15:19:57 -0400

    Changed some layout on the About Me page. Took out the icons of the technologies and added a list of names of the technologies. Divided the technologies into frontend, backend, and fullstack.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,8 +6,6 @@ import './About.scss';
 import image1 from '../../assets/images/image-4.png';
 import image2 from '../../assets/images/image-5.png';
 
-import Techlogos from '../../assets/images/tech-logo.png'
-
 const About = () => {
     return (
         <div className="about-container">
@@ -32,9 +30,16 @@ const About = () => {
                 <h1>About Me</h1>
                 <div className="about-description">
                     <p>I’m Ruth Bhatti, a software engineer and project manager based in New York City. I have a Bachelor’s degree in Criminal Justice Management from John Jay College (CUNY) and certifications as a Software Quality Engineer (CSQE) and a Cosmetologist. I have experience in data entry and analysis, customer service, and cosmetology. I am passionate about developing innovative applications and working on impactful projects.</p>
-                    <p>In my personal life, I am a devoted dog mom to my two beloved dogs, Olivia and Buddy. My journey into software engineering began with a Bootcamp at BrainStation, where I learned</p>
-                    <img className="about-techlogos" src={Techlogos} alt="Node.js, React, HTML5, CSS3, JavaScript, MySQL, VS Code, Git, OpenAI, GitHub, Atlassian Jira, Heroku, Kraken.js, Sass, npm, Express.js, and Jenkins." />
-                    <p>For my capstone project, PawVoyage, I mastered React Native in just two weeks. This experience underscores my ability to quickly learn and adapt to new technologies, demonstrating my readiness to tackle challenging projects and continuously grow as a professional.</p>
+                    <p>In my personal life, I am a devoted dog mom to my two beloved dogs, Olivia and Buddy. My journey into software engineering began with a Bootcamp at BrainStation, where I learned the following technologies:</p>
+                    <div className="technology-list">
+                        <h3>Front-end Technologies:</h3>
+                        <p>HTML5, CSS3, JavaScript, React, Sass, TypeScript, Vite, Redux, Figma, Axios, Testing Library, React Router</p>
+                        <h3>Back-end Technologies:</h3>
+                        <p>Node.js, Express, MySQL, MongoDB, Kraken</p>
+                        <h3>Full-stack Technologies:</h3>
+                        <p>Git, GitHub, npm, yarn, Jira, Heroku, Android Studio</p>
+                    </div>
+                    <p>For my capstone project, PawVoyage, I learned React Native in just two weeks. This experience underscores my ability to quickly learn and adapt to new technologies, demonstrating my readiness to tackle challenging projects and continuously grow as a professional.</p>
                 </div>
             </div>
             <div className="desktop-carousel">
